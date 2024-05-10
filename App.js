b2934bf2f380bbd5d89401a0f2ed6f64d096046f
anasrasulmq <68836529+anasrasulmq@users.noreply.github.com>
2024-05-10 15:20:43 +1000

    Add files via upload
    ADDED:
    - Each picker does not auto-scroll back to its default value, it keeps the value the user changes it to.
    - a (currently non-functional) button

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
-import { SafeAreaView, Text, View, StyleSheet, Image } from 'react-native';
-import Images from './assets/assets'
+import { SafeAreaView, Text, View, StyleSheet, Image, Button } from 'react-native';
+import {useState} from "react";
+import Images from './assets/assets.js';
 import {Picker} from '@react-native-picker/picker';
 
+
 export default function App() {
+  const [selectedP1, setSelectedP1] = useState('');
+  const [selectedP2, setSelectedP2] = useState('');
+  const [selectedP3, setSelectedP3] = useState('');
+  const [selectedP4, setSelectedP4] = useState('');
   return (
       <SafeAreaView style={styles.container}>
         <View style={styles.container}>
@@ -15,42 +21,63 @@ export default function App() {
             source={Images.logo} 
           />
           <View style = {styles.row1}>
-            <Picker style={styles.vegPicker}>
-              <Picker.Item label="Select Veg" value="0" color="white"/>
-              <Picker.Item label="Apple-$11" value="11" color="white"/>
-              <Picker.Item label="Orange-$11" value="11" color="white"/>
-              <Picker.Item label="Banana-$5" value="4" color="white"/>
-              <Picker.Item label="Blueberry-$12" value="12" color="white"/>
-              <Picker.Item label="Mango-$8" value="8" color="white"/>
+            <Picker style={styles.vegPicker}
+            selectedValue={selectedP1}
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedP1(itemValue)
+            }>
+              <Picker.Item label="Select Veg" value="0" />
+              <Picker.Item label="Apple-$11" value="11" />
+              <Picker.Item label="Orange-$11" value="11" />
+              <Picker.Item label="Banana-$5" value="4" />
+              <Picker.Item label="Blueberry-$12" value="12" />
+              <Picker.Item label="Mango-$8" value="8" />
             </Picker>
-            <Picker style={styles.vegPicker}>
-              <Picker.Item label="Select Amount" value="0" color="white"/>
-              <Picker.Item label="1" value="1" color="white"/>
-              <Picker.Item label="2" value="2" color="white"/>
-              <Picker.Item label="3" value="3" color="white"/>
-              <Picker.Item label="4" value="4" color="white"/>
-              <Picker.Item label="5" value="5" color="white"/>
+            <Picker style={styles.vegPicker}
+            selectedValue={selectedP2}
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedP2(itemValue)
+            }>
+            
+              <Picker.Item label="Select Amount" value="0"/>
+              <Picker.Item label="1" value="1" />
+              <Picker.Item label="2" value="2" />
+              <Picker.Item label="3" value="3" />
+              <Picker.Item label="4" value="4" />
+              <Picker.Item label="5" value="5"/>
             </Picker>
           </View>
 
           <View style = {styles.row1}>
-            <Picker style={styles.fruitPicker}>
-              <Picker.Item label="Select Fruit" value="0" color="white"/>
-              <Picker.Item label="Apple-$11" value="11" color="white"/>
-              <Picker.Item label="Orange-$11" value="11" color="white"/>
-              <Picker.Item label="Banana-$5" value="4" color="white"/>
-              <Picker.Item label="Blueberry-$12" value="12" color="white"/>
-              <Picker.Item label="Mango-$8" value="8" color="white"/>
+            <Picker style={styles.fruitPicker}
+            selectedValue={selectedP3}
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedP3(itemValue)
+            }>
+              <Picker.Item label="Select Fruit" value="0"/>
+              <Picker.Item label="Apple-$11" value="11" />
+              <Picker.Item label="Orange-$11" value="11" />
+              <Picker.Item label="Banana-$5" value="4" />
+              <Picker.Item label="Blueberry-$12" value="12" />
+              <Picker.Item label="Mango-$8" value="8" />
             </Picker>
-            <Picker style={styles.fruitPicker}>
-              <Picker.Item label="Select Amount" value="0" color="white"/>
-              <Picker.Item label="1" value="1" color="white"/>
-              <Picker.Item label="2" value="2" color="white"/>
-              <Picker.Item label="3" value="3" color="white"/>
-              <Picker.Item label="4" value="4" color="white"/>
-              <Picker.Item label="5" value="5" color="white"/>
+            <Picker style={styles.fruitPicker}
+            selectedValue={selectedP4}
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedP4(itemValue)
+            }>
+              <Picker.Item label="Select Amount" value="0" />
+              <Picker.Item label="1" value="1" />
+              <Picker.Item label="2" value="2" />
+              <Picker.Item label="3" value="3" />
+              <Picker.Item label="4" value="4" />
+              <Picker.Item label="5" value="5" />
             </Picker>
           </View>
+      </View>
+      <View marginTop='30%'>
+        <Button title="CALCULATE" />
+        <Text style={styles.heading}> Placeholder </Text>
       </View>
     </SafeAreaView>
   );
@@ -74,9 +101,6 @@ const styles = StyleSheet.create({
     width: 128,
     marginLeft:'36%',
     marginRight:'30%'
-  },
-  pickerText: {
-    color:"white"
   },
   row1: {
     flexDirection:'row',
